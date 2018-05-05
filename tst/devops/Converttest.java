35caf84e32a9ddb14e856ca493f8dca8b8e96252
shantha <shantha@DESKTOP-FGIMUUS>
2018-05-05 11:39:09 +0530

    Modfied Code

---
+++ tst/devops/Converttest.java
@@ -0,0 +1,23 @@
+package devops;
+
+import org.hamcrest.core.Is;
+import org.junit.*;
+
+
+public class Converttest 
+{
+	
+	@Test
+	public void ConverttoWords1() {
+		
+		Assert.assertThat(Converter.Convert(1), Is.is("One"));
+	}
+	
+	@Test
+	public void ConverttoWords2() {
+		
+		Assert.assertThat(Converter.Convert(2), Is.is("Two"));
+	}
+	
+	}
+	
